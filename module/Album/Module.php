1eeb9590ddb2553a2d02ba2b6116489907439377
Tomek <n1kula@wp.pl>
2015-01-06 13:10:04 +0100

    Album model files

--- module/Album/Module.php
+++ module/Album/Module.php
@@ -8,20 +8,20 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface
 {
     public function getAutoloaderConfig()
     {
-    return array(
-         ’Zend\Loader\ClassMapAutoloader’ => array(
-            __DIR__ . ’/autoload_classmap.php’,
-        ),
-        ’Zend\Loader\StandardAutoloader’ => array(
-            ’namespaces’ => array(
-                __NAMESPACE__ => __DIR__ . ’/src/’ . __NAMESPACE__,
-           ),
-         ),
-      );
-    }
+     return array(
+             'Zend\Loader\ClassMapAutoloader' => array(
+                 __DIR__ . '/autoload_classmap.php',
+             ),
+             'Zend\Loader\StandardAutoloader' => array(
+                 'namespaces' => array(
+                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                 ),
+             ),
+         );
+     }
 
 public function getConfig()
     {
-    return include __DIR__ . ’/config/module.config.php’;
+    return include __DIR__ . '/config/module.config.php';
     }
 }
