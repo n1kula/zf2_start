0f53c1605756d524014d12028ae4799ab9d511ba
Tomek <n1kula@wp.pl>
2015-02-02 18:16:11 +0100

    tutorial close

--- module/Album/Module.php
+++ module/Album/Module.php
@@ -1,33 +1,22 @@
 <?php
 namespace Album;
 
-use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
-use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Album\Model\Album;
 use Album\Model\AlbumTable;
 use Zend\Db\ResultSet\ResultSet;
 use Zend\Db\TableGateway\TableGateway;
 
-class Module implements AutoloaderProviderInterface, ConfigProviderInterface
-{
-    public function getAutoloaderConfig()
-    {
-     return array(
-             'Zend\Loader\ClassMapAutoloader' => array(
-                 __DIR__ . '/autoload_classmap.php',
-             ),
-             'Zend\Loader\StandardAutoloader' => array(
-                 'namespaces' => array(
-                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                 ),
-             ),
-         );
+class Module 
+ {
+     public function getAutoloaderConfig()
+     {
+     
      }
 
-    public function getConfig()
-    {
-    return include __DIR__ . '/config/module.config.php';
-    }
+     public function getConfig()
+     {
+         return include __DIR__ . '/config/module.config.php';
+     }
 
      function getServiceConfig()
      {
--- module/Album/src/Album/Model/Album.php
+++ module/Album/src/Album/Model/Album.php
@@ -2,6 +2,10 @@
 
 namespace Album\Model;
 
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\InputFilterAwareInterface;
+use Zend\InputFilter\InputFilterInterface;
+
  class Album
  {
      public $id;
@@ -14,4 +18,70 @@ namespace Album\Model;
          $this->artist = (!empty($data['artist'])) ? $data['artist'] : null;
          $this->title  = (!empty($data['title'])) ? $data['title'] : null;
      }
- }
+     
+     public function getArrayCopy()
+     {
+         return get_object_vars($this);
+     }
+     
+// Add content to these methods:
+    public function setInputFilter(InputFilterInterface $inputFilter)
+    {
+        throw new \Exception("Not used");
+    }
+
+    public function getInputFilter()
+    {
+        if (!$this->inputFilter) {
+            $inputFilter = new InputFilter();
+
+            $inputFilter->add(array(
+                'name'     => 'id',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'Int'),
+                ),
+            ));
+
+            $inputFilter->add(array(
+                'name'     => 'artist',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            ));
+
+            $inputFilter->add(array(
+                'name'     => 'title',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            ));
+            $this->inputFilter = $inputFilter;
+        }
+        return $this->inputFilter;
+    }
+}
